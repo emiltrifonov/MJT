c236fc996520ecd0ac73ee032bbf0971c6c307bd
emiltrifonov <em4ito@gmail.com>
2025-10-12 22:40:09 +0300

    add solution to task 1 of lab 1

---
+++ Week01/lab/UniqueSubstringFinder.java
@@ -0,0 +1,35 @@
+public class UniqueSubstringFinder {
+    private static int getTrackerArrayIndex(char[] stringArr, int ind) {
+        return stringArr[ind] - 'a';
+    }
+
+    public static String longestUniqueSubstring(String s) {
+        char[] arr = s.toCharArray();
+        boolean[] charAppearanceTracker = new boolean[26];
+        int l = 0, maxLength = 0;
+        String res = s;
+
+        for (int i = 0; i < arr.length; i++) {
+            int ind = getTrackerArrayIndex(arr, i);
+
+            if (charAppearanceTracker[ind]) {
+                int currentLength = i - l;
+                if (currentLength > maxLength) {
+                    maxLength = currentLength;
+                    res = s.substring(l, i);
+                }
+
+                while (arr[l] != arr[i]) {
+                    charAppearanceTracker[getTrackerArrayIndex(arr, l)] = false;
+                    l++;
+                }
+                l++;
+            }
+            else {
+                charAppearanceTracker[ind] = true;
+            }
+        }
+
+        return res;
+    }
+}
