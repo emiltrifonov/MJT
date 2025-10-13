4f0120d5870ea615e2848272215069802155669f
emiltrifonov <em4ito@gmail.com>
2025-10-13 14:31:43 +0300

    brr

---
+++ Week01/lab/TaskDistributor/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(TaskDistributor.minDifference(new int[]{1,2,3,4,5}));
+        System.out.println(TaskDistributor.minDifference(new int[]{10,20,15,5}));
+        System.out.println(TaskDistributor.minDifference(new int[]{7,3,2,1,5,4}));
+        System.out.println(TaskDistributor.minDifference(new int[]{9,1,1,1}));
+        System.out.println(TaskDistributor.minDifference(new int[]{}));
+        System.out.println(TaskDistributor.minDifference(new int[]{120}));
+        System.out.println(TaskDistributor.minDifference(new int[]{30, 30}));
+    }
+}
