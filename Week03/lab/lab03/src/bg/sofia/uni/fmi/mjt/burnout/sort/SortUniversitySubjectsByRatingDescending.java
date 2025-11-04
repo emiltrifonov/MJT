a0cc58aae694ea7782fbada0c672d9661d191a7a
emiltrifonov <103438524+emiltrifonov@users.noreply.github.com>
2025-11-04 13:22:03 +0200

    Update SortUniversitySubjectsByRatingDescending.java

--- Week03/lab/lab03/src/bg/sofia/uni/fmi/mjt/burnout/sort/SortUniversitySubjectsByRatingDescending.java
+++ Week03/lab/lab03/src/bg/sofia/uni/fmi/mjt/burnout/sort/SortUniversitySubjectsByRatingDescending.java
@@ -5,8 +5,8 @@ import bg.sofia.uni.fmi.mjt.burnout.subject.UniversitySubject;
 import java.util.Arrays;
 
 public final class SortUniversitySubjectsByRatingDescending implements SortUniversitySubjects{
-        @Override
-        public UniversitySubject[] execute(UniversitySubject[] subjects) {
+    @Override
+    public UniversitySubject[] execute(UniversitySubject[] subjects) {
         UniversitySubject[] sorted = Arrays.copyOf(subjects, subjects.length);
 
         for (int i = 0; i < subjects.length; i++) {
