4e7334af2d7bfd029ef415853ec1e7a88c8fbc26
emiltrifonov <em4ito@gmail.com>
2025-11-15 17:12:51 +0200

    idk

--- Week01/lab/UniqueSubstringFinder/src/UniqueSubstringFinder.java
+++ Week01/lab/UniqueSubstringFinder/src/UniqueSubstringFinder.java
@@ -1,3 +1,7 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 public class UniqueSubstringFinder {
     private static int getTrackerArrayIndex(char[] stringArr, int ind) {
         return stringArr[ind] - 'a';
